ae7c8623fc2f1b5b6a974c34210bee3ea8f7155e
DesignC <znehraks@gmail.com>
2020-04-24 03:04:25 +0900

    urls views setting

---
+++ RestaurantShare/shareRes/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('restaurantDetail/',views.restaurantDetail),
+    path('restaurantCreate/',views.restaurantCreate),
+    path('categoryCreate/',views.categoryCreate),
+]
--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    # return HttpResponse("index")
+    return render(request, 'shareRes/index.html')
+
+def restaurantDetail(request):
+    # return HttpResponse("restaurantDetail")
+    return render(request, 'shareRes/restaurantDetail.html')
+
+def restaurantCreate(request):
+    # return HttpResponse("restaurantCreate")
+    return render(request, 'shareRes/restaurantCreate.html')
+
+def categoryCreate(request):
+    # return HttpResponse("categoryCreate")
+    return render(request, 'shareRes/categoryCreate.html')
